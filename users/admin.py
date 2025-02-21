b8a05acba2fba38a7eda3150c2be839574d2017c
Brannon <brannon@mohara.co>
2025-02-21 09:20:19 +0200

    feat(users): :sparkles: register ParkingUser model in admin site with some custom options

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ParkingUser
 
-# Register your models here.
+class ParkingUserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'license_plate', 'client')
+    list_filter = ('client',)
+    search_fields = ('first_name', 'last_name', 'email', 'license_plate')
+
+admin.site.register(ParkingUser, ParkingUserAdmin)
