22219db88545d01596453e2bc3c81f90ad3d7fc4
Brannon <brannon@mohara.co>
2025-02-21 11:49:41 +0200

    feat(users): :sparkles: add custom permissions and queryset filtering to ParketUserAdmin ParketUserAdmin
    - this ensures only administrator employees can see and manage only their clients ParketUsers

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 from .models import ParketUser
 
-class ParkingUserAdmin(admin.ModelAdmin):
+class ParketUserAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'license_plate', 'client')
     list_filter = ('client',)
     search_fields = ('first_name', 'last_name', 'email', 'license_plate')
 
-admin.site.register(ParketUser, ParkingUserAdmin)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(client__administrators=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "client" and not request.user.is_superuser:
+            kwargs["queryset"] = request.user.administered_clients.all()
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def has_change_permission(self, request, obj=None):
+        if not obj or request.user.is_superuser:
+            return True
+        return request.user in obj.client.administrators.all()
+
+    def has_delete_permission(self, request, obj=None):
+        return self.has_change_permission(request, obj)
+
+admin.site.register(ParketUser, ParketUserAdmin)
