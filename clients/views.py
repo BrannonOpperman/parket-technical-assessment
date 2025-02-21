0fef3c3668253ae12f23b5cc8edca7e962c58ccd
Brannon <brannon@mohara.co>
2025-02-21 14:54:36 +0200

    feat(clients): :sparkles: implement ClientViewSet with restricted permissions and HTTP methods

--- clients/views.py
+++ clients/views.py
@@ -1,5 +1,17 @@
-from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .serializers import ClientSerializer
 
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class = ClientSerializer
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the clients index.")
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return []
+
+    # limit the allowed functionality to POST requests for creating new clients only
+    http_method_names = ['post']
+
+    def perform_create(self, serializer):
+        serializer.save()
