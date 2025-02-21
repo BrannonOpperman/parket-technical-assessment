a5c373d2187d1904c972ca770727196071cb2c1a
Brannon <brannon@mohara.co>
2025-02-21 16:11:57 +0200

    feat(users): :sparkles: add client_id field to ParketUserSerializer
    - this allows validation on administrators

--- users/serializers.py
+++ users/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from .models import ParketUser
+from clients.models import Client
 
 class ParketUserSerializer(serializers.ModelSerializer):
+    client_id = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = ParketUser
-        fields = ['id', 'first_name', 'last_name', 'email', 'license_plate']
-        read_only_fields = ['id', 'client']
+        fields = ['id', 'first_name', 'last_name', 'email', 'license_plate', 'client_id']
+        read_only_fields = ['id']
+
+    def validate_client_id(self, value):
+        # Ensure the client exists and that the user is an administrator for them
+        try:
+            client = Client.objects.get(id=value)
+            if not client.administrators.filter(id=self.context['request'].user.id).exists():
+                raise serializers.ValidationError("You are not an administrator for this client")
+            return value
+        except Client.DoesNotExist:
+            raise serializers.ValidationError("Invalid client ID")
