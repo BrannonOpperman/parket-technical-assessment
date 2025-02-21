5367b73b23590c083560ee25bdcf027dc383bb01
Brannon <brannon@mohara.co>
2025-02-21 16:12:28 +0200

    feat(users): :sparkles: add programmatic client assignment during ParketUser creation

--- users/views.py
+++ users/views.py
@@ -2,11 +2,15 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import ParketUser
 from .serializers import ParketUserSerializer
+from clients.models import Client
 
 class ParketUserViewSet(viewsets.ModelViewSet):
     serializer_class = ParketUserSerializer
     permission_classes = [IsAuthenticated]
 
-    # Filter users by the client of the authenticated user
     def get_queryset(self):
         return ParketUser.objects.filter(client__administrators=self.request.user)
+
+    def perform_create(self, serializer):
+        client = Client.objects.get(id=serializer.validated_data['client_id'])
+        serializer.save(client=client)
