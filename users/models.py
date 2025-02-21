e27c0da94bb4ca8db9bf22c581ee5b00a9ca6d0a
Brannon <brannon@mohara.co>
2025-02-21 09:16:52 +0200

    feat(users): :card_file_box: add ParkingUser model
    - this represents the users of the system relating to a specific client

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ParkingUser(models.Model):
+    """User model for the parking system"""
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.EmailField('Email address')
+    license_plate = models.CharField(max_length=20)
+    client = models.ForeignKey(
+        'clients.Client',
+        on_delete=models.CASCADE,
+        related_name='parking_users'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ['email', 'client']
+        verbose_name = 'Parking User'
+        verbose_name_plural = 'Parking Users'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - ({self.license_plate})"
