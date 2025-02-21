eb2404c74d12223be036dbbef2d95c0ec54c1a70
Brannon <brannon@mohara.co>
2025-02-21 11:51:42 +0200

    feat(clients): :sparkles: add custom permissions and queryset filtering to ClientsAdmin
    - this ensures client users only can see their own administrators and ParketUsers unless they have superuser privilege

--- clients/admin.py
+++ clients/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
 from .models import Client
 
-admin.site.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'created_at')
+    filter_horizontal = ('administrators',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(administrators=request.user)
+
+    def has_change_permission(self, request, obj=None):
+        if not obj or request.user.is_superuser:
+            return True
+        return request.user in obj.administrators.all()
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return False
+
+admin.site.register(Client, ClientAdmin)
