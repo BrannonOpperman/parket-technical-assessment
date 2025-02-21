cc8796b17a0072a64b805684e6104aa8c8d2cdc6
Brannon <brannon@mohara.co>
2025-02-21 11:36:29 +0200

    fix(users): :truck: update admin registration to use ParketUser model

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import ParkingUser
+from .models import ParketUser
 
 class ParkingUserAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'license_plate', 'client')
     list_filter = ('client',)
     search_fields = ('first_name', 'last_name', 'email', 'license_plate')
 
-admin.site.register(ParkingUser, ParkingUserAdmin)
+admin.site.register(ParketUser, ParkingUserAdmin)
